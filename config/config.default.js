62be58130f1e4f2f4c5879103362d63551593c08
Gold <770294561@qq.com>
2023-06-09 17:32:19 +0800

    feat:完成了egg的jwt鉴权部分

---
+++ config/config.default.js
@@ -0,0 +1,59 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1686289495394_7739';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    domainWhiteList: [ '*' ],
+  };
+
+  config.view = {
+    mapping: { '.html': 'ejs' },
+  };
+
+  config.jwt = {
+    secret: 'ViGil',
+  };
+
+  exports.mysql = {
+    client: {
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: '770294561',
+      database: 'juejue-cost',
+    },
+    app: true,
+    agent: false,
+  };
+
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
