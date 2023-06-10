3203e4f17caba6a249fd933dc3b4e5ad2ab621bc
Gold <770294561@qq.com>
2023-06-10 17:25:27 +0800

    feat:前端项目设置完毕

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import routes from '@/router'
+function App() {
+  return <Router>
+    <Routes>
+      {
+        routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component/>} />)
+      }
+    </Routes>
+  </Router>
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createStyleImportPlugin } from "vite-plugin-style-import";
+import path from 'path'
+const __dirname = path.resolve()
+
+export default defineConfig({
+    plugins: [
+        react(),
+        createStyleImportPlugin({
+            libs: [
+                {
+                    libraryName: "zarm",
+                    esModules: true,
+                    resolveStyle: (name) => {
+                        return `zarm/es/${name}/style/css`;
+                    },
+                },
+            ],
+        }),
+    ],
+    css: {
+      modules: {
+        localsConvention: 'dashesOnly'
+      },
+      preprocessorOptions: {
+        less: {
+          // 支持内联 JavaScript
+          javascriptEnabled: true,
+        }
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:7001/api/',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
+      }
+    },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // src 路径
+      'utils': path.resolve(__dirname, 'src/utils') // src 路径
+    }
+  },
+});
